38665c1b5ad7b0e6f87858cdcbce960c485b4fb8
Ganfan-Ren <80506840+Ganfan-Ren@users.noreply.github.com>
2021-08-04 20:49:37 +0800

    Update main.py

--- capture_photo/main.py
+++ capture_photo/main.py
@@ -4,9 +4,12 @@ import cv2
 print('press \'s\': capture\npress \'q\': exit\n')
 
 cap=cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 while(1):
     ret,picture = cap.read()
-    cv2.imshow('picture',picture)
+    reSize = cv2.resize(picture, (2560//2,720//2), interpolation=cv2.INTER_CUBIC)
+    cv2.imshow('capture photo',reSize)
     k = cv2.waitKey(10)
     if k == ord('s'):
         Photocut.save_img(picture)
