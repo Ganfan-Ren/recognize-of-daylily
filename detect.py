29f4f76f7ebd6db0fa37a5f02ef54aa38a31cf58
Zhaojx <Zhao.jx123@qq.com>
2022-11-21 19:59:22 +0800

    add nms file

--- detect.py
+++ detect.py
@@ -1,12 +1,22 @@
 import torch
 import cv2
+import yaml
 import numpy as np
+from model import DayHeap
+from utils import Detres
 
-def detect(net,img,config):
-    pass
+def detect(net,img,config,threshold):
+    size = config['size_img']
+    img = cv2.resize(img,[size[1],size[0]])
+    inp = torch.from_numpy(img.transpose([2,0,1])).unsqueeze(0).type(torch.float)
+    det = net(inp)
+    res_al = tensor_objabsvalue(det,config) # al are angle and length
+    detecter = Detres(res_al,config,threshold)
+    keypoint = detecter.getkeypoint()
+    return keypoint
 
-def tensor_objabsvalue(det,config): # 将输出转换为[600+2400,9]
-    y1,y2,y3 = det
+def tensor_objabsvalue(det,config): # 将输出转换为[2400+600,9]
+    y1,y2,heapmap = det
     imgsize = config['size_img']
     long_cls = config['long_class']
     angle_cls = np.array(config['angle'])
@@ -18,7 +28,8 @@ def tensor_objabsvalue(det,config): # 将输出转换为[600+2400,9]
     cls_angle = y1[1].transpose(1, 3).contiguous().view(-1, 3)
     cls_index = torch.argmax(cls_angle,1).view(-1,1)  # [600,1]
     cls_onehot = torch.zeros([cls_index.shape[0],len(angle_cls)]) # [600,3]
-    cls_onehot = cls_onehot.scatter(1,cls_index,torch.ones_like(cls_index))
+    cls_temp = torch.ones_like(cls_index).type(torch.float)
+    cls_onehot = cls_onehot.scatter(1,cls_index,cls_temp)
     cls_tensor = torch.from_numpy(angle_cls)
     related_angle = y1[2][:, 3:, :, :].contiguous().view(-1, 3)
     angle0 = related_angle[:,0] + torch.sum(cls_tensor * cls_onehot,1) # [600,1]
@@ -35,7 +46,7 @@ def tensor_objabsvalue(det,config): # 将输出转换为[600+2400,9]
     xy_tensor = torch.cat(
         [height / size[0] * (x_forcal + y1[3][:, 0, :, :]), width / size[1] * (y_forcal + y1[3][:, 1, :, :])], 1)
     xy_tensor = xy_tensor.transpose(1, 3).contiguous().view(-1, 2) # [600,2]
-    y1_res = torch.cat([obj_conf,xy_tensor,angle0,angle1,angle2,length0,length1,length2],1)
+    y1_res = torch.cat([obj_conf,xy_tensor,angle0.view(-1,1),angle1.view(-1,1),angle2.view(-1,1),length0.view(-1,1),length1.view(-1,1),length2.view(-1,1)],1)
 
     # --------------针对y2的计算------------
     size = y2[0].squeeze().shape  # [20,30] or [40,60]
@@ -44,28 +55,39 @@ def tensor_objabsvalue(det,config): # 将输出转换为[600+2400,9]
     cls_angle = y2[1].transpose(1, 3).contiguous().view(-1, 3) # [2400,3]
     cls_index = torch.argmax(cls_angle, 1).view(-1, 1)  # [2400,1]
     cls_onehot = torch.zeros([cls_index.shape[0], len(angle_cls)])  # [2400,3]
-    cls_onehot = cls_onehot.scatter(1, cls_index, torch.ones_like(cls_index))
+    cls_temp = torch.ones_like(cls_index).type(torch.float)
+    cls_onehot = cls_onehot.scatter(1, cls_index, cls_temp)
     cls_tensor = torch.from_numpy(angle_cls) # [3]
-    related_angle = y1[2][:, 3:, :, :].contiguous().view(-1, 3) # [2400,3]
+    related_angle = y2[2][:, 3:, :, :].contiguous().view(-1, 3) # [2400,3]
     angle0 = related_angle[:, 0] + torch.sum(cls_tensor * cls_onehot, 1)  # [2400,1]
     angle1 = angle0 - related_angle[:, 1]
     angle2 = related_angle[:, 2] - angle0
     # ---------------长度-----------------
-    related_length = y1[2][:, :3, :, :].contiguous().view(-1, 3)
-    length0 = related_length[:, 0] * long_cls[0]
+    related_length = y2[2][:, :3, :, :].contiguous().view(-1, 3)
+    length0 = related_length[:, 0] * long_cls[1]
     length1 = related_length[:, 1] * length0
     length2 = related_length[:, 2] * length0
     # ---------------坐标-----------------
     x_forcal, y_forcal = torch.linspace(0, size[0] - 1, size[0]).view(1, 1, size[0], 1), \
                          torch.linspace(0, size[1] - 1, size[1]).view(1, 1, 1, size[1])
     xy_tensor = torch.cat(
-        [height / size[0] * (x_forcal + y1[3][:, 0, :, :]), width / size[1] * (y_forcal + y1[3][:, 1, :, :])], 1)
+        [height / size[0] * (x_forcal + y2[3][:, 0, :, :]), width / size[1] * (y_forcal + y2[3][:, 1, :, :])], 1)
     xy_tensor = xy_tensor.transpose(1, 3).contiguous().view(-1, 2)  # [600,2]
-    y1_res = torch.cat([obj_conf, xy_tensor, angle0, angle1, angle2, length0, length1, length2], 1)
+    y2_res = torch.cat([obj_conf, xy_tensor, angle0.view(-1,1), angle1.view(-1,1), angle2.view(-1,1), length0.view(-1,1), length1.view(-1,1), length2.view(-1,1)], 1)
+    y = torch.cat([y1_res,y2_res],0)
+    return y,heapmap
 
 
 def main():
-    pass
+    net = DayHeap()
+    with open('config/config.yaml','r') as f:
+        config = yaml.load(f,yaml.FullLoader)
+    imgpath = r'F:\daylily_w\dataset\image\IMGdaylily_03274.jpg'
+    img = cv2.imread(imgpath)
+    t = torch.load(config['weight_path'])
+    net.load_state_dict(t)
+    res = detect(net,img,config,0.3)
+    print(res)
 
 if __name__ == '__main__':
-    pass
+    main()
--- utils/SoaNMS.py
+++ utils/SoaNMS.py
@@ -1,6 +1,86 @@
 import torch
 import numpy as np
 
-class Detres():
-    def __init__(self,keypoints):
-        pass
+class Detres(): # detect result
+    def __init__(self,res_al,config,threshold):
+        self.res_al = res_al[0]
+        self.heaepmap = res_al[1]
+        self.config = config
+        self.threshold = threshold
+        self.index = []
+        self.isNMS = False
+
+    def NMS(self,SOAthreshold=0.5):
+        res_thre = []
+        for i,obj in enumerate(self.res_al):
+            if obj[0] >= self.threshold:
+                res_thre.append(obj.unsqueeze(0))
+                self.index.append(i)
+        # 排序
+        res_temp = torch.cat(res_thre,0)
+        res_thre,index = res_temp.sort(0,True) # True 为从大到小
+        ind_temp = []
+        for i in index:
+            ind_temp.append(self.index[i[0]])
+        self.index = ind_temp
+        res_tensor = []
+        i = 0
+        while i < len(res_thre)-1:
+            j = i+1
+            while j < len(res_thre):
+                if self.SOA(res_thre[i],res_thre[j],self.config['T']) > SOAthreshold: # 越大越接近
+                    res_thre = torch.cat([res_thre[:j-1,:],res_thre[j:,:]])
+                    self.index.pop(j)
+                    continue
+                j += 1
+            i += 1
+        self.isNMS = True
+        self.res_al = res_thre
+        return res_thre
+
+
+    def SOA(self,x1,x2,T=0.5): # [obj_conf, x, y, angle0, angle1, angle2, length0, length1, length2]
+        # info=[x,y,angle,length]             O —— ——>x
+        # x = x + torch.cos(angle) * length   |
+        # y = y - torch.sin(angle) * length  y|
+        cal_x = lambda x: int(x[0] + torch.cos(x[2]) * x[3])
+        cal_y = lambda x: int(x[1] - torch.sin(x[2]) * x[3])
+        cal_xy = lambda x:(cal_x(x),cal_y(x))  # 跟据角度和长度计算坐标
+        cal_length = lambda x:np.sqrt((x[1][1] - x[0][1])**2 + (x[1][0] - x[0][0])**2)
+        cal_sigma = lambda x: 0 if x < T else 1
+        (x1_2,y1_2) = (int(x1[2]),int(x1[1]))
+        (x1_1,y1_1) = cal_xy([x1_2,y1_2,x1[4]-np.pi,x1[7]])
+        (x1_3, y1_3) = cal_xy([x1_2, y1_2, x1[3], x1[6]])
+        (x1_4, y1_4) = cal_xy([x1_3, y1_3, x1[5], x1[8]])
+        keypoint1 = [(x1_1,y1_1),(x1_2,y1_2),(x1_3, y1_3),(x1_4, y1_4)]
+        long1 = torch.sum(x1[6:])
+        (x2_2, y2_2) = (int(x2[2]), int(x2[1]))
+        (x2_1, y2_1) = cal_xy([x2_2, y2_2, x2[4] - np.pi, x2[7]])
+        (x2_3, y2_3) = cal_xy([x2_2, y2_2, x2[3], x2[6]])
+        (x2_4, y2_4) = cal_xy([x2_3, y2_3, x2[5], x2[8]])
+        keypoint2 = [(x2_1, y2_1),(x2_2, y2_2),(x2_3, y2_3),(x2_4, y2_4)]
+        long2 = torch.sum(x2[6:])
+        long = long1 if x1[0] > x2[0] else long2
+        sumd_ij = 0
+        for i,p in enumerate(keypoint1):
+            sumd_ij += cal_length([p,keypoint2[i]])
+        LP_single = torch.exp(-sumd_ij/long*cal_sigma(sumd_ij/long))
+        acc_alpha = 0 if 5 * torch.max(torch.abs(x1[3:6]-x2[3:6])) > 1 else 1 - 5 * torch.max(torch.abs(x1[3:6]-x2[3:6]))
+        SOA = LP_single * acc_alpha
+        return SOA
+
+    def getkeypoint(self):
+        if not self.isNMS:
+            self.NMS(self.config['SOA'])
+        keypoints = []
+        cal_x = lambda x: int(x[0] + torch.cos(x[2]) * x[3])
+        cal_y = lambda x: int(x[1] - torch.sin(x[2]) * x[3])
+        cal_xy = lambda x: (cal_x(x), cal_y(x))  # 跟据角度和长度计算坐标
+        for i,obj in enumerate(self.res_al):
+            (x2, y2) = (int(obj[2]), int(obj[1]))
+            (x1, y1) = cal_xy([x2, y2, obj[4] - np.pi, obj[7]])
+            (x3, y3) = cal_xy([x2, y2, obj[3], obj[6]])
+            (x4, y4) = cal_xy([x3, y3, obj[5], obj[8]])
+            keypoint = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+            keypoints.append(keypoint)
+        return keypoints
